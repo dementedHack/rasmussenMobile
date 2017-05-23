71c63be4b947481c52f04e7f0edc70ff334cee7c
dementedHack <kaiperez@yahoo.com>
2017-05-22 21:29:00 -0700

    .log

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 var models = {};
-var connectionString = "";
+var connectionString = "mongodb://mobiledb:GUd0MBA1Cf7XMWdJ6FvCsyCEOZW6W1062pg6V6KhLyPmhveQVhd3YMkq8s2N4BuvecnsH3KKCazGlfLGSUEyBg==@mobiledb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
 
 mongoose.connect(connectionString);
+
+console.log('working');
