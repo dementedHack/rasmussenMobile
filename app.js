d4991af2fa2ad26a2f5b06af7c1f635d5d04e587
dementedHack <kaiperez@yahoo.com>
2017-05-22 21:02:54 -0700

    Mongoose install

--- app.js
+++ app.js
@@ -5,31 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// SQL connections
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-
-// Create connection to database
-var config = {
-  userName: 'kai', // update me
-  password: 'Keepitreal20', // update me
-  server: 'kaimobiledb.database.windows.net' // update me
-  // options: {
-  //     database: 'your_database' //update me
-  // }
-}
-var connection = new Connection(config);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) {
-    if (err) {
-        console.log(err)
-    }
-    else{
-        alert("Working!")
-    }
-});
-
 function queryDatabase(){
     console.log('Reading rows from the Table...');
 
