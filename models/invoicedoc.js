fc1e542967a72ab4818e6efec5140ce5978f82ff
CodeByKai <kaiperez.dev@gmail.com>
2017-05-23 09:25:32 -0700

    d

--- models/invoicedoc.js
+++ models/invoicedoc.js
@@ -2,10 +2,10 @@
 var mongoose = require('mongoose');
 
 // Define our beer schema
-var InvoiceScema   = new mongoose.Schema({
-  name: String,
+var InvoiceSchema   = new mongoose.Schema({
+  taskName: String,
   type: String
 });
 
 // Export the Mongoose model
-module.exports = mongoose.model('Invoice', InvoiceScema);
+module.exports = mongoose.model('Invoice', InvoiceSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,39 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 var models = {};
+var Invoice = require('../models/invoicedoc');
+
 var connectionString = "mongodb://mobiledb:GUd0MBA1Cf7XMWdJ6FvCsyCEOZW6W1062pg6V6KhLyPmhveQVhd3YMkq8s2N4BuvecnsH3KKCazGlfLGSUEyBg==@mobiledb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
 
 mongoose.connect(connectionString);
 
-var Invoice = require('../models/invoicedoc');
+var db = mongoose.connection;
+
+db.once('open', function() {
+	console.log('connected to DB successfully');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kais Frustration' });
 
-  Invoice.find(function(err, invoices) {
-    if (err)
-      res.send(err);
-
-    res.json(invoices);
-  });
-
-  console.log('working');
+	  Invoice.find(function (err, invoices) {
+	  if (err) return console.error(err);
+	  console.log(invoices);
+	})
 });
+
+router.post('/', function(){
+
+	var newInvoice = new Invoice;
+	newInvoice.taskName = "The task";
+	newInvoice.type = "PDF";
+
+	newInvoice.save(function(err){
+		if(err){
+			console.log(err);
+		}
+	})
+})
 
 module.exports = router;
