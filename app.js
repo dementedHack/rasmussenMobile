bb8794c84d1353d9acdd7fc7a1701dfda6a839fb
dementedHack <kaiperez@yahoo.com>
2017-05-22 20:27:27 -0700

    took out

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
 // Create connection to database
-var config = {
-  userName: 'kai', // update me
-  password: 'Keepitreal20', // update me
-  server: 'kaimobiledb.database.windows.net' // update me
-  // options: {
-  //     database: 'your_database' //update me
-  // }
-}
-var connection = new Connection(config);
+// var config = {
+//   userName: 'kai', // update me
+//   password: 'Keepitreal20', // update me
+//   server: 'kaimobiledb.database.windows.net' // update me
+//   // options: {
+//   //     database: 'your_database' //update me
+//   // }
+// }
+// var connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
 // connection.on('connect', function(err) {
