99b54ba5fb6beeb39b985a3822cb70246d73ad24
dementedHack <kaiperez@yahoo.com>
2017-05-22 20:23:13 -0700

    Changed DB

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // SQL connections
-// var Connection = require('tedious').Connection;
-// var Request = require('tedious').Request;
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-// // Create connection to database
-// var config = {
-//   userName: 'kai', // update me
-//   password: 'Keepitreal20', // update me
-//   server: 'advancedmobilerasmussen.database.windows.net', // update me
-//   // options: {
-//   //     database: 'your_database' //update me
-//   // }
-// }
+// Create connection to database
+var config = {
+  userName: 'kai', // update me
+  password: 'Keepitreal20', // update me
+  server: 'kaimobiledb.database.windows.net', // update me
+  // options: {
+  //     database: 'your_database' //update me
+  // }
+}
 // var connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
